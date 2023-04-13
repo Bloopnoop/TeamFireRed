e4c7109e925f13d1a2ce53c1edae1e21b08c25e6
ZackGau <125404303+ZackGau@users.noreply.github.com>
2023-04-13 10:55:36 -0500

    Update Notes.php

--- phpChanges/Notes.php
+++ phpChanges/Notes.php
@@ -15,13 +15,13 @@
 		<div class="collapse navbar-collapse" id="sidebar">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-					<a class="nav-link" href="MainProjectPage.php">Home</a>
+					<a class="nav-link" href="MainProjectPage.html">Home</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="AddStudent.php">Add Student</a>
+					<a class="nav-link" href="AddStudent.html">Add Student</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="Notes.php">Notes</a>
+					<a class="nav-link" href="thebigtest2.html">Notes</a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="#">Templates</a>
@@ -29,7 +29,6 @@
 			</ul>
 		</div>
 	</nav>
-</head>
 </head>
 <body style="background-color: #58131c;">
 
@@ -48,14 +47,30 @@
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script>
-		$(document).ready(function() {
+	$(document).ready(function() {
+		// Retrieve saved notes from Local Storage
+		if(localStorage.getItem("notes")){
+			$('.notes').html(localStorage.getItem("notes"));
+		}
+		
+		// Save note to Local Storage and display
 		$('#save-button').on('click', function() {
 			var note = $('#note-input').val();
 			if (note) {
 				var timestamp = new Date().toLocaleString();
-				$('.notes').append('<div class="note">' + note + '<br><span class="timestamp">' + timestamp + '</span></div>');
+				$('.notes').append('<div class="note">' + note + '<br><span class="timestamp">' + timestamp + '</span><button class="delete-button">Delete</button></div>');
 				$('#note-input').val('');
+				// Save notes to Local Storage
+				localStorage.setItem("notes", $('.notes').html());
 			}
+		});
+		
+		// Add event listener to delete buttons
+		$('.notes').on('click', '.delete-button', function() {
+			// Remove note from DOM
+			$(this).closest('.note').remove();
+			// Remove note from local storage
+			localStorage.setItem("notes", $('.notes').html());
 		});
 	});
 </script>
